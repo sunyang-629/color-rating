b7d693785e8982504b116a30558d7a1b7e36fa80
sunyang9876 <sunyang9876@hotmail.com>
2019-12-27 22:44:48 +1000

    complete addcolorform

--- src/addColorForm/AddColorForm.js
+++ src/addColorForm/AddColorForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './../stylesheets/AddColorForm.scss'
+import './styles/AddColorForm.scss'
 
 class AddColorForm extends React.Component {
     static propTypes = {
