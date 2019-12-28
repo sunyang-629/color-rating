ce86a3267f0d5b77b3641a23c24f2f294d7196b6
sunyang9876 <sunyang9876@hotmail.com>
2019-12-28 22:04:10 +1000

    add delete button

--- src/colorList/components/Color.js
+++ src/colorList/components/Color.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import StarRating from './StarRating';
+import PropTypes from 'prop-types';
 import './../styles/Color.scss';
 
 const Color = ({title,color,rating=0}) => {
     return (
         <section className='color'>
             <h1>{title}</h1>
+            <button>X</button>
             <div className='color' style={{backgroundColor:color}}>
             </div>
             <div>
@@ -13,6 +15,10 @@ const Color = ({title,color,rating=0}) => {
             </div>
         </section>
     )
+}
+
+Color.propTypes = {
+    rating:PropTypes.number
 }
 
 export default Color;
